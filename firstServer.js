ca80cfb9d296b3795fbf1fb432f31ac2d0a6338c
Regie Marzan <lonebeginner@gmail.com>
2023-04-10 19:08:43 +0800

    add aboutMe route

--- firstServer.js
+++ firstServer.js
@@ -11,6 +11,11 @@ app.get('/', function(req, res){
 app.get('/contact',function(req,res){
     res.send("Hi, please do contact me at : lonebeginner.com");
 });
+
+app.get('/aboutMe',function(req,res){
+    res.send("I'm a freelance javascript developer. I interesting code");
+});
+
 app.listen(3000,function(){
     console.log('Test Port 3000')
 });
