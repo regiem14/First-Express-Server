175daeface7d170f72b73061937bb133548609b7
Regie Marzan <lonebeginner@gmail.com>
2023-04-10 19:02:16 +0800

    add contact route

--- firstServer.js
+++ firstServer.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(request, res){
-    console.log(request);
+app.get('/', function(req, res){
+    // console.log(request);
     // console.log(typeof request);
-    // res.send('<h1>Hello World. This is my <em>firstSever</em> using Express</h1>')
+    res.send('<h1>Hello World. This is my <em>firstSever</em> using Express</h1>')
 })
+
+app.get('/contact',function(req,res){
+    res.send("Hi, please do contact me at : lonebeginner.com");
+});
 app.listen(3000,function(){
     console.log('Test Port 3000')
 });
